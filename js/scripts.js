1167270aaef66700c2d30d1931ca6df912473644
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-17 19:47:31 -0700

    updated js to have cleaner code and remove console logs
    Co-authored-by: Brycen Bartolome <brycenbartolome@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,31 @@
 $(document).ready(function() {
   
   const clubs = " of clubs";
-  const diamonds = " of diamonds"
-  const hearts = " of hearts"
-  const spades = " of spades"
-
+  const diamonds = " of diamonds";
+  const hearts = " of hearts";
+  const spades = " of spades";
+  // const cardType = [" of clubs", " of diamonds", " of hearts", " of spades"];
   const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"];
-  let allClubs = [];
+ 
+  
+  // cards.forEach(function(element1) {
+  //   cardType.forEach(function(element2) {
+  //     $("#list").append("<li>" + element1 + element2 + "</li>");
+  //   });
+  // });
   cards.forEach(function(element) {
-      allClubs.push(element + clubs);
-      console.log(allClubs);
       $("#list").append("<li>" + element + clubs + "</li>");
   });
-
-  let allDiamonds = [];
+  
   cards.forEach(function(element) {
-      allClubs.push(element + diamonds);
-      console.log(allClubs);
       $("#list").append("<li>" + element + diamonds + "</li>");
   });
 
-  let allHearts = [];
   cards.forEach(function(element) {
-      allClubs.push(element + hearts);
-      console.log(allClubs);
       $("#list").append("<li>" + element + hearts + "</li>");
   });
 
-  let allSpades = [];
   cards.forEach(function(element) {
-      allClubs.push(element + spades);
-      console.log(allClubs);
       $("#list").append("<li>" + element + spades + "</li>");
   });
-
-
-  event.preventDefault();
 });
